6ffa18bbd01286bee27962f3e08ae3be64781e9c
Arunadevi-codes <aruganesan03@gmail.com>
2026-04-06 13:53:55 +0530

    Added frontend files properly

---
+++ frontend/src/services/customerService.js
@@ -0,0 +1,11 @@
+import API from "./api";
+
+export const getCustomers = () => API.get("/customers");
+
+export const addCustomer = (data) => API.post("/customers", data);
+
+export const updateCustomer = (id, data) =>
+  API.put(`/customers/${id}`, data);
+
+export const deleteCustomer = (id) =>
+  API.delete(`/customers/${id}`);
